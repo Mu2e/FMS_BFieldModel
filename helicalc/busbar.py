Made small fix to handle geometry dataframes with non-numeric columns. This is needed for settting up rotation functions in integrators.

helicalc/busbar.py
@@ -53,7 +53,8 @@ class StraightIntegrator3D(object):
             self.zps = lib.linspace(0, self.L, abs(int(self.L/self.dxyz[2] + 1)))
         self.XP, self.YP, self.ZP = lib.meshgrid(self.xps, self.yps, self.zps, indexing='ij')
         # rotation
-        self.euler2 = geom_df[['Phi2', 'theta2', 'psi2']].values
+        # self.euler2 = geom_df[['Phi2', 'theta2', 'psi2']].values
+        self.euler2 = np.array([geom_df.Phi2, geom_df.theta2, geom_df.psi2])
         self.rot = Rotation.from_euler('zyz', self.euler2[::-1], degrees=True)
         self.inv_rot = self.rot.inv()
 
@@ -226,7 +227,8 @@ class ArcIntegrator3D(object):
         self.COSPHI = lib.cos(self.PHI)
         self.RHOP = self.rho - self.YP
         # rotations
-        self.euler2 = geom_df[['Phi2', 'theta2', 'psi2']].values
+        # self.euler2 = geom_df[['Phi2', 'theta2', 'psi2']].values
+        self.euler2 = np.array([geom_df.Phi2, geom_df.theta2, geom_df.psi2])
         self.rot = Rotation.from_euler('zyz', self.euler2[::-1], degrees=True)
         self.inv_rot = self.rot.inv()
 

helicalc/coil.py
@@ -121,7 +121,8 @@ class CoilIntegrator(object):
         self.lib = lib
         self.dev = dev
         # rotation function
-        self.XYZ_rot = geom_coil[[f'rot{i:d}' for i in [0,1,2]]].values
+        # self.XYZ_rot = geom_coil[[f'rot{i:d}' for i in [0,1,2]]].values
+        self.XYZ_rot = np.array([geom_coil[f'rot{i:d}'] for i in [0,1,2]])
         self.XYZ_rot_rad = np.radians(self.XYZ_rot)
         self.mu2e_to_coil = Rotation.from_euler('XYZ', -self.XYZ_rot_rad)
         self.coil_to_mu2e = self.mu2e_to_coil.inv()