19c834bbd23d2eaed2bbd968076ba7bd031519b6
Cole Kampa <ckampa13@gmail.com>
2024-05-08 12:58:40 -0500

    helicalc refactored.

--- helicalc_package/setup.py
+++ helicalc_package/setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     description="Biot-Savart integration for helical solenoids.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/FMS-Mu2e/helicalc",
+    url="https://github.com/Mu2e/FMS_BFieldModel/helicalc_package",
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy", "scipy", "pandas", "matplotlib"
